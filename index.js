3cf086e68277a13357dc31767ac655e81f2dfaa7
Santosh Shaastry <csshaastry@gmail.com>
2015-02-06 02:23:55 +0530

    Support for Ctrl+P (Print)

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var Handlebars = require("handlebars");
 
 function render(resume) {
 	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+	var print = fs.readFileSync(__dirname + "/print.css", "utf-8");
 	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
 	return Handlebars.compile(template)({
 		css: css,
+		print: print,
 		resume: resume
 	});
 }
